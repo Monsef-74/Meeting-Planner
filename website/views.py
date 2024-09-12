32725790a572df0d1bd13c1471f06b7f0e624d94
unknown <m7mdmonsef@gmail.com>
2024-09-13 01:38:13 +0300

    project meeting planner (Django)

---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from meetings.models import Meeting
+
+def welcome(request):
+    return render(request,'website/index.html', 
+                  {"meetings": Meeting.objects.all()})
+
+def about(request):
+    return HttpResponse('I am Monsef')
+
+def members(request):
+    return HttpResponse('you are the only memeber here , you are alone !')
